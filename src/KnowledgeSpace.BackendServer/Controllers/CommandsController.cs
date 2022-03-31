4837bac17829dcdc3987717fa86522a6ef0e54dd
baovn-2976 <74024040+baovn-2976@users.noreply.github.com>
2022-03-31 16:46:39 +0300

    new app web test optimization

---
+++ src/KnowledgeSpace.BackendServer/Controllers/CommandsController.cs
@@ -0,0 +1,36 @@
+﻿using KnowledgeSpace.BackendServer.Data;
+using KnowledgeSpace.ViewModels.Systems;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KnowledgeSpace.BackendServer.Controllers
+{
+    public class CommandsController : BaseController
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CommandsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetCommands()
+        {
+            // gan them 20 quyen khac nhau cho user (10 quyen cua identity, 10 quyen gan them
+            var user = User.Identity.Name;
+            // sử dung AsNoTracking -< chỉ đọc mà không ghi --> Tối ưu hóa việc truy cập dữ liệu
+            var commands = _context.Commands.AsNoTracking();
+
+            var commandVms = await commands.Select(u => new CommandVm()
+            {
+                Id = u.Id,
+                Name = u.Name,
+            }).ToListAsync();
+
+            return Ok(commandVms);
+        }
+    }
+}
---
+++ src/KnowledgeSpace.WebPortal/Extensions/IdentityExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace KnowledgeSpace.WebPortal.Extensions
+{
+    public static class IdentityExtensions
+    {
+        public static string GetUserId(this ClaimsPrincipal claimsPrincipal)
+        {
+            /*var claim = ((ClaimsIdentity)claimsPrincipal.Identity)
+                .Claims
+                .SingleOrDefault(x => x.Type == ClaimTypes.NameIdentifier);
+            return claim.Value;*/
+
+
+            var claim = ((ClaimsIdentity)claimsPrincipal.Identity).Claims.SingleOrDefault(x => x.Type == ClaimTypes.NameIdentifier);
+            if (claim == null)
+            {
+                //return Challenge(new AuthenticationProperties { RedirectUri = "/" }, "oidc");
+                //AccountController login = new AccountController(claim);
+                return "";
+
+            }
+            else
+            {
+                return claim.Value;
+            }
+        }
+    }
+}
